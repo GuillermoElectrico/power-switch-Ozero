d03006ea00498bc3555f247d6951a84fb50ff76e
GuillermoElectrico <guillermoelectrico@outlook.com>
2018-07-14 16:01:31 +0200

    Add led status

--- power-switch-Ozero.py
+++ power-switch-Ozero.py
@@ -5,10 +5,15 @@ import subprocess
 GPIO.setmode(GPIO.BOARD)
 
 #Select unused GPIO header pin to be used for shutdown
-InputPin = 12 
+InputPin = 12 #PA07/PA_EINT7/SIM_CLK
+LedOnPin = 13 #PA00/UART2_TX
 
 # Set selected pin to input, need pullup resistor external in 3.3V and pin select.
 GPIO.setup(InputPin, GPIO.IN)
+# Set selected pin to output.
+GPIO.setup(LedOnPin, GPIO.OUT)
+
+GPIO.output(LedOnPin, 1)
 
 if GPIO.input(InputPin):
     print('Input was HIGH')
